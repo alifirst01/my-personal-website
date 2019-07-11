3edf81d3bfb7af4b62e7c9fdd8556defcfea4535
Ali Asgher <ali-asgher@uiowa.edu>
2019-07-11 16:55:04 -0500

    Created sidebar component with links to github, linkedin, email and handshake profile

---
+++ src/components/Sidebar.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import "styles/themes/_sidebar.sass";
+import handshake from "assets/images/icons/handshake.png";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub, faLinkedin } from '@fortawesome/fontawesome-free-brands';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+
+class Sidebar extends Component{
+    constructor(props, context) {
+        super(props, context);
+        this.state = {};
+    }
+
+    render() {
+        return(
+            <div className="sidebar">
+                <ul>
+                    <li><FontAwesomeIcon icon={faEnvelope} className="icon" style={{color: "#ff2f33"}}/></li>
+                    <li><FontAwesomeIcon icon={faGithub} className="icon"/></li>
+                    <li><FontAwesomeIcon icon={faLinkedin} className="icon" style={{color: "#0077B5"}}/></li>
+                    <li><img height="25px" src={handshake}/></li>
+                </ul>
+            </div>
+        )
+    }
+}
+
+export default Sidebar;
