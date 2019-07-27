6bf129156d28063b5fe492c377d0d2c515609c70
Ali Asgher <ali-asgher@uiowa.edu>
2019-07-27 18:48:42 -0500

    Added home button to navbar and linked navitems to their respective div tags

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
-import Button from '@material-ui/core/Button';
 import "../styles/themes/_navbar.sass";
+import {faHome} from '@fortawesome/free-solid-svg-icons';
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Button from "react-bootstrap/Button";
 
 class Navbar extends Component{
     constructor(props, context) {
@@ -18,27 +22,43 @@ class Navbar extends Component{
 
     handleScroll = () => {
         var scroll = window.scrollY;
-        if(scroll > 0)
-            document.getElementsByClassName("custom-navbar")[0].classList.add("active");
-        else {
-            console.log(document.getElementsByClassName("custom-navbar")[0].classList);
-            document.getElementsByClassName("custom-navbar")[0].classList.remove("active");
-            console.log(document.getElementsByClassName("custom-navbar")[0].classList);
+        var element = document.getElementsByClassName("custom-navbar")[0];
+        if(!element.classList.contains('active') && scroll > 0)
+            element.classList.add("active");
+        else if(scroll === 0 && element.classList.contains('active'))
+            element.classList.remove("active");
+    };
+
+    handleClick = (e, tag) => {
+        e.preventDefault();
+        if(this.props.home === true){
+            this.props.switchHome();
+            setTimeout(function () {
+                document.querySelector(tag).scrollIntoView({ top: 500, behavior: 'smooth' });
+            }, 1000);
         }
+        else
+            document.querySelector(tag).scrollIntoView({ top: 500, behavior: 'smooth' });
     };
 
     render() {
         return(
-            <div className="custom-navbar">
-                <ul className="navlist">
-                    <li className="link"><a href="">About</a></li>
-                    <li className="link"><a href="">Projects</a></li>
-                    <li className="link"><a href="">Skills</a></li>
-                    <li className="link"><a href="">Experience</a></li>
-                    <li className="link"><a href="">Contact</a></li>
-                    <li><Button variant="outlined" id="resume-button">Resume</Button></li>
-                </ul>
-            </div>
+            <Row className="custom-navbar">
+                <Col md={2} style={{textAlign: "left"}}>
+                    <Button className="home-icon" variant="link" onClick={this.props.backToHome}>
+                        <FontAwesomeIcon icon={faHome} />
+                    </Button>
+                </Col>
+                <Col md={10} style={{textAlign: "right"}}>
+                    <ul className="navlist">
+                        <li className="link"><a href="#about" onClick={() => this.handleClick(event, "#about")}>About</a></li>
+                        <li className="link"><a href="#projects" onClick={() => this.handleClick(event, "#projects")}>Projects</a></li>
+                        <li className="link"><a href="#employment" onClick={() => this.handleClick(event, "#employment")}>Experience</a></li>
+                        <li className="link"><a href="#contact" onClick={() => this.handleClick(event, "contact")}>Contact</a></li>
+                        <li><Button id="resume-button">Resume</Button></li>
+                    </ul>
+                </Col>
+            </Row>
         )
     }
 }
