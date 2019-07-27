95a623f52df97a19041ee722b19d2251f254cd6d
Ali Asgher <ali-asgher@uiowa.edu>
2019-07-27 18:47:48 -0500

    Added mouse wheel event on homepage and removed arrow slide animation

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,21 @@
 import React, { Component } from 'react';
 import "../styles/themes/_home.sass"
-import Button from '@material-ui/core/Button';
 import Fade from 'react-reveal/Fade';
+import Button from "react-bootstrap/Button";
 
 class Home extends Component{
     constructor(props, context) {
         super(props, context);
         this.state = {};
     }
+
+    componentDidMount = () => {
+        window.addEventListener('wheel', this.props.updateContent);
+    };
+
+    componentWillUnmount() {
+        window.removeEventListener('wheel', this.props.updateContent);
+    };
 
     render() {
         return(
@@ -19,28 +27,9 @@ class Home extends Component{
                     <p style={{width: "40%", paddingLeft: "5%"}}>specializes in building quality web applications with highly maintainable and testable code</p>
 
                     <div>
-                        <Button id="explore-button" onClick={this.props.updateContent} variant="outlined">
-                            <span>
-                                Explore more
-                            </span>
-                            <span>
-                                <div id="arrowAnim">
-                                    <div className="arrowSliding">
-                                        <div className="arrow"> </div>
-                                    </div>
-                                    <div className="arrowSliding delay1">
-                                        <div className="arrow"> </div>
-                                    </div>
-                                    <div className="arrowSliding delay2">
-                                        <div className="arrow"> </div>
-                                    </div>
-                                    <div className="arrowSliding delay3">
-                                        <div className="arrow"> </div>
-                                    </div>
-                                </div>
-                            </span>
+                        <Button id="explore-button" onClick={this.props.updateContent} variant="outline-success">
+                            Explore more
                         </Button>
-
                     </div>
                 </Fade>
             </div>
